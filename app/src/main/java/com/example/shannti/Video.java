fe04bc9a3265e91d84c83c39f68df2c6215aea24
lcm1011 <lcm9073@naver.com>
2023-12-07 01:36:24 +0900

    .

---
+++ app/src/main/java/com/example/shannti/Video.java
@@ -0,0 +1,65 @@
+package com.example.shannti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class Video extends AppCompatActivity {
+    VideoView vv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+
+        vv= findViewById(R.id.vv);
+        //Video Uri
+        Uri videoUri=Uri.parse("android.resource://"+getPackageName() + "/"+ R.raw.squat);
+
+        //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
+        vv.setMediaController(new MediaController(this));
+
+        //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
+        vv.setVideoURI(videoUri);
+
+        //동영상을 읽어오는데 시간이 걸리므로..
+        //비디오 로딩 준비가 끝났을 때 실행하도록..
+        //리스너 설정
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //비디오 시작
+                vv.start();
+            }
+        });
+
+        // 비디오 재생이 끝나면 다시 처음부터 재생
+        vv.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                // 비디오 다시 시작
+                vv.seekTo(0);
+                vv.start();
+            }
+        });
+    }
+
+    //화면에 안보일때...
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //비디오 일시 정지
+        if(vv!=null && vv.isPlaying()) vv.pause();
+    }
+    //액티비티가 메모리에서 사라질때..
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //
+        if(vv!=null) vv.stopPlayback();
+    }
+}
